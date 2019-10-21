26933cef731e85527bceae202f6c5d375ffd10bd
opqi <petr.dmitriev.96@gmail.com>
2019-10-21 19:27:20 +0300

    init commit has some changes how to make many-to-many a scaleable choice

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+
+    def __unicode__(self):
+        return self.type
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+
+    def __unicode__(self):
+        return self.type
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # categories = models.ManyToManyField('Category', related_name='posts', through='Category')
+    categories = models.ForeignKey('Post', related_name='posts', on_delete=models.CASCADE)
+    # categories = models.ManyToOneRel('Category', related_name='posts')
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
